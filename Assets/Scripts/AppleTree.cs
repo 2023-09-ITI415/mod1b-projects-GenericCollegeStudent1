df2253f978b8c71ed01f6dc5242c1ce3549bf234
GenericCollegeStudent1 <144394893+GenericCollegeStudent1@users.noreply.github.com>
2023-09-26 20:58:45 -0400

    not pog

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-public class ApplePicker : MonoBehaviour
+public class AppleTree : MonoBehaviour
 {
-    [Header("Set In Inspector")]
+
+
+    [Header("Set in Inspector")]
+
     public GameObject applePrefab;
-
     public float speed = 1f;
-
     public float leftAndRightEdge = 10f;
-
     public float chanceToChangeDirections = 0.1f;
-
     public float secondsBetweenAppleDrops = 1f;
-    // Start is called before the first frame update
 
+
+    // Start is called before the first frame update
     void Start()
     {
+
+        // Dropping apples every second
+
         Invoke("DropApple", 2f);
+
     }
 
     void DropApple()
@@ -26,32 +31,41 @@ public class ApplePicker : MonoBehaviour
         GameObject apple = Instantiate<GameObject>(applePrefab);
         apple.transform.position = transform.position;
         Invoke("DropApple", secondsBetweenAppleDrops);
-
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += speed * Time.deltaTime;
-        transform.position = pos;
+
+        //tree movement
+
+        Vector3 pos = transform.position;       // current position
+        pos.x += speed * Time.deltaTime;        // change in time * speed
+        transform.position = pos;               // pos becomes new location of tree
+
+        // changing directions
 
         if (pos.x < -leftAndRightEdge)
         {
+            // are we at the left-most side?
             speed = Mathf.Abs(speed);
         }
         else if (pos.x > leftAndRightEdge)
         {
+            // are we at the right-most side?
             speed = -Mathf.Abs(speed);
         }
 
     }
 
-    // Update is called once per frame
-    void fixedUpdate()
+    void FixedUpdate()
     {
+        // Changing Direction Randommly is now time-based because of FixedUpdate()
+
         if (Random.value < chanceToChangeDirections)
         {
-            speed *= -1;
+            speed *= -1; // Change direction
         }
     }
+
 }
